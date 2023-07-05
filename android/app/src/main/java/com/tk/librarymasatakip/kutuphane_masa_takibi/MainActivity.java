7368ba3eafdce0dce10df708a3feb411153caa31
tugcekepen <kepentugce@gmail.com>
2023-07-05 23:10:05 +0300

    first commit

---
+++ android/app/src/main/java/com/tk/librarymasatakip/kutuphane_masa_takibi/MainActivity.java
@@ -0,0 +1,6 @@
+package com.tk.librarymasatakip.kutuphane_masa_takibi;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/home.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightGreenAccent,
+        shadowColor: Colors.lightGreenAccent,
+        title: Text("Kütüphanem"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+          ],
+        ),
+      ),
+    );
+  }
+}
