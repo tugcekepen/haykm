0fc94b6af76298528df516beb924c252305e1ddc
tugcekepen <kepentugce@gmail.com>
2023-07-07 00:51:54 +0300

    Taslak üzerinde çalışıldı. Geçici eklemeler, Ana sayfa arayüzü.

--- lib/home.dart
+++ lib/home.dart
@@ -10,15 +10,77 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.lightGreenAccent,
-        shadowColor: Colors.lightGreenAccent,
-        title: Text("Kütüphanem"),
+        backgroundColor: Color(0xFFCE0D44),
+        title: Text(
+          "Hasan Ali Yücel Kültür Merkezi",
+          style: TextStyle(fontSize: 18.5),
+        ),
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.menu),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.person_rounded),
+          )
+        ],
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-
+            ClipOval(
+              child: Image.asset(
+                'assets/photo_2023-07-07_00-07-07.jpg',
+                width: 300,
+              ),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height / 35),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                "Kurslar",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              ),
+              style: ElevatedButton.styleFrom(
+                  shape: StadiumBorder(),
+                  fixedSize: Size(200, 40)),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height / 35),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                "Kafeterya Menüsü",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              ),
+              style: ElevatedButton.styleFrom(
+                  shape: StadiumBorder(),
+                  fixedSize: Size(200, 40)),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height / 35),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                "Kütüphane",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              ),
+              style: ElevatedButton.styleFrom(
+                  shape: StadiumBorder(),
+                  fixedSize: Size(200, 40)),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height / 35),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                "??",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              ),
+              style: ElevatedButton.styleFrom(
+                  shape: StadiumBorder(),
+                  fixedSize: Size(200, 40)),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height / 15),
           ],
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:kutuphane_masa_takibi/home.dart';
+import 'package:json_theme/json_theme.dart';
 
-void main() {
-  runApp(MyApp());
+import 'package:flutter/services.dart'; // For rootBundle
+import 'dart:convert';
+
+import 'package:kutuphane_masa_takibi/home.dart'; // For jsonDecode
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final themeStr = await rootBundle.loadString('assets/appainter_theme.json');
+  final themeJson = jsonDecode(themeStr);
+  final theme = ThemeDecoder.decodeThemeData(themeJson)!;
+
+  runApp(MyApp(theme: theme));
 }
 
 class MyApp extends StatelessWidget {
+  final ThemeData theme;
+
+  const MyApp({Key? key, required this.theme}) : super(key: key);
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage(),
-    );
+    return MaterialApp(home: MyHomePage(), theme: theme);
   }
 }
 
