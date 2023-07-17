35d93f93789247a055ddc5ba82a8b1281e65d429
Yaser61 <kalkanyaser@gmail.com>
2023-07-17 15:48:15 +0300

    events page uptadet

--- lib/pages/events_page.dart
+++ lib/pages/events_page.dart
@@ -15,7 +15,8 @@ class _EventsPage extends State<EventsPage> {
 
   List<Events> events = [
     Events("Stajyer Öğrencilerle Buluştuk", image: 'assets/images/sicakicecekler.jpg'),
-    Events("s", image: 'assets/images/meşrubatlar.jpg'),
+    Events("s", image: 'assets/images/mesrubat.jpg'),
+    Events("Şok Şok Şok", image: 'assets/images/menemen.jpg')
     //İSTEDİĞİN KADAR EKLE
   ];
 
@@ -36,17 +37,35 @@ class _EventsPage extends State<EventsPage> {
       body: ListView.builder(
         itemCount: events.length,
         itemBuilder: (BuildContext context, int index){
-          Events event = Events(event[index]);
+          Events event = events[index];
           return ListTile(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => EventScreen(event: events[index])),
+              );
+            },
             title: Center(
-              child: Text(event.title), // Etkinlik başlığını görüntüleyin
+              child: Column(
+                children: [
+                  if(events[index].image != null)
+                    Image.asset(
+                        events[index].image!,
+                            width: 300,
+                            height: 250,
+                            fit: BoxFit.cover,
+                    ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                        events[index].title,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
             ),
-            leading: event.image != null ? Row(
-              children: [
-                Image.network(event.image!),
-              ]
-
-            ) : null,
           );
         },
       ),
@@ -60,4 +79,36 @@ class Events {
   final String? image;
 
   Events(this.title, {this.image});
+}
+
+
+
+
+
+class EventScreen extends StatelessWidget {
+  final GlobalKey<ScaffoldState> _scaffold = GlobalKey<ScaffoldState>();  final Events event;
+
+  EventScreen({super.key, required this.event});
+
+  void drawerOpen() {
+      _scaffold.currentState?.openDrawer();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffold,
+      drawer: DrawerMenu(),
+      appBar: CustomAppBar(
+          scaffold: _scaffold,
+          title: "Haberin Olsun",
+          icon: Icons.menu,
+          onIconPressed: drawerOpen),
+      body: Column(
+        children: [
+          Text(event.title),
+        ],
+      ),
+    );
+  }
 }
